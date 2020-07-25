d7f0c0a148daa53d9a131127c552a40f43ea8ec4
Tamara Kyrychenko <tamara.kyrychenko@gmail.com>
2020-07-25 18:22:03 +0300

    Implemented taking db connection data from local environment settings.

--- src/main/java/com/softserve/edu/jom/MySQLConfig.java
+++ src/main/java/com/softserve/edu/jom/MySQLConfig.java
@@ -1,20 +1,27 @@
 package com.softserve.edu.jom;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:local.db.properties")
 public class MySQLConfig {
+    @Autowired
+    private Environment env;
 
     @Bean
     public DataSource datasource() {
         return DataSourceBuilder.create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/marathon?useUnicode=true&serverTimezone=UTC")
-                .username("Taras")
-                .password("11111")
+                .driverClassName(env.getRequiredProperty("datasource.driverClassName"))
+                .url(env.getRequiredProperty("datasource.url"))
+                .username(env.getRequiredProperty("datasource.username"))
+                .password(env.getRequiredProperty("datasource.password"))
                 .build();
     }
 }
