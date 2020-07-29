2de80ccfc76199284b409c4e1a501233a9b43f24
TarasDovganyuk <exscee@gmail.com>
2020-07-29 16:14:01 +0300

    views merged, styles changed

--- src/main/java/com/softserve/edu/jom/controller/MarathonController.java
+++ src/main/java/com/softserve/edu/jom/controller/MarathonController.java
@@ -1,6 +1,5 @@
 package com.softserve.edu.jom.controller;
 
-
 import com.softserve.edu.jom.model.Marathon;
 import com.softserve.edu.jom.model.User;
 import com.softserve.edu.jom.service.MarathonService;
@@ -34,43 +33,43 @@ public class MarathonController {
     }
 
     @GetMapping("/delete/{marathonId}")
-    public String deleteMarathon(@PathVariable(name="marathonId") Long id) {
+    public String deleteMarathon(@PathVariable(name = "marathonId") Long id) {
         marathonService.deleteMarathonById(id);
         return "redirect:/marathons";
     }
 
     @GetMapping("/edit/{marathonId}")
-    public String editMarathon(Model model, @PathVariable(name="marathonId") Long id) {
-       Marathon marathon = marathonService.getMarathonById(id);
-       model.addAttribute("marathon", marathon);
-        return "editMarathon";
+    public String editMarathon(Model model, @PathVariable(name = "marathonId") Long id) {
+        Marathon marathon = marathonService.getMarathonById(id);
+        model.addAttribute("marathon", marathon);
+        model.addAttribute("mode", "Edit");
+        return "changeMarathon";
     }
 
     @PostMapping("/edit/{marathonId}")
-    public String editMarathon(Model model, @PathVariable(name="marathonId") Long id,
+    public String editMarathon(Model model, @PathVariable(name = "marathonId") Long id,
                                @ModelAttribute(name = "marathon") Marathon marathon) {
         marathon.setId(id);
         marathonService.createOrUpdate(marathon);
         return "redirect:/marathons";
-    };
-
+    }
 
     @GetMapping("/addMarathon")
     public String addMarathon(Model model) {
         Marathon marathon = new Marathon();
         model.addAttribute("marathon", marathon);
-        return "addMarathon";
+        model.addAttribute("mode", "Add");
+        return "changeMarathon";
     }
 
-    @PostMapping("/saveMarathon")
-    public String addMarathon(@ModelAttribute(name="marathon") Marathon marathon) {
+    @PostMapping("/addMarathon")
+    public String addMarathon(@ModelAttribute(name = "marathon") Marathon marathon) {
         marathonService.createOrUpdate(marathon);
-
         return "redirect:/marathons";
     }
 
     @GetMapping("/users/{marathonId}")
-    public String getAllUsers(Model model, @PathVariable(name="marathonId") Long id) {
+    public String getAllUsers(Model model, @PathVariable(name = "marathonId") Long id) {
         Set<User> users = marathonService.getMarathonById(id).getUsers();
         model.addAttribute("users", users);
         return "students";
--- src/main/java/com/softserve/edu/jom/service/MarathonServiceImpl.java
+++ src/main/java/com/softserve/edu/jom/service/MarathonServiceImpl.java
@@ -1,15 +1,12 @@
 package com.softserve.edu.jom.service;
 
-import com.softserve.edu.jom.exception.MarathonServiceException;
 import com.softserve.edu.jom.model.Marathon;
 import com.softserve.edu.jom.repository.MarathonRepository;
-import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -38,22 +35,12 @@ public class MarathonServiceImpl implements MarathonService {
     }
 
     @Override
-    public Marathon createOrUpdate(Marathon marathon)  {
-        System.out.println(marathon);
+    public Marathon createOrUpdate(Marathon marathon) {
         if (marathon.getId() != null) {
-
             Marathon existedMarathon = getMarathonById(marathon.getId());
             existedMarathon.setTitle(marathon.getTitle());
             return marathonRepository.save(existedMarathon);
-//            Optional<Marathon> marathonOptional = marathonRepository.findById(marathon.getId());
-//
-//            if (marathonOptional.isPresent()) {
-//                Marathon newMarathon = marathonOptional.get();
-//                newMarathon.setTitle(marathon.getTitle());
-//                return marathonRepository.save(newMarathon);
-//            }
         }
-
         return marathonRepository.save(marathon);
     }
 
