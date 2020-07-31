1d8643577a3a34181a635ed40463a4aef7200075
TarasDovganyuk <exscee@gmail.com>
2020-07-31 17:41:47 +0300

    unused import removed

--- src/test/java/com/softserve/edu/jom/repository/MarathonRepositoryTest.java
+++ src/test/java/com/softserve/edu/jom/repository/MarathonRepositoryTest.java
@@ -5,7 +5,6 @@ import com.softserve.edu.jom.model.Marathon;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,5 +19,4 @@ public class MarathonRepositoryTest {
         Marathon actual = marathonRepository.getMarathonById(1L);
         assertEquals("Java Online Marathon", actual.getTitle());
     }
-
 }
