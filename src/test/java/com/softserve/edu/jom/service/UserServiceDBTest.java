feb6e151873f1bb03c684248dc6ce86eee26489b
Tamara Kyrychenko <tamara.kyrychenko@gmail.com>
2020-08-01 20:16:31 +0300

    Fixed tests

--- src/test/java/com/softserve/edu/jom/service/UserServiceDBTest.java
+++ src/test/java/com/softserve/edu/jom/service/UserServiceDBTest.java
@@ -1,6 +1,5 @@
 package com.softserve.edu.jom.service;
 
-import com.softserve.edu.jom.exception.UserServiceException;
 import com.softserve.edu.jom.model.Marathon;
 import com.softserve.edu.jom.model.User;
 import org.junit.jupiter.api.Test;
@@ -15,15 +14,15 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest
 @AutoConfigureTestDatabase
 @Transactional
 @AutoConfigureTestEntityManager
 public class UserServiceDBTest {
     private UserService userService;
     private TestEntityManager entityManager;
+    private static final String COUNT_QUERY_STRING = "select count(x) from %s x";
 
     @Autowired
     public void setUserService(UserService userService) {
@@ -49,12 +48,18 @@ public class UserServiceDBTest {
         User.Role role = User.Role.MENTOR;
         user.setRole(role);
         Long marathonId = 1L;
+        long initialUserCount = count("User");
         boolean isAdded = userService.addUserToMarathon(user, marathonId);
+        entityManager.flush();
         assertTrue(isAdded);
+        long currentUserCount = count("User");
+        assertEquals(initialUserCount + 1, currentUserCount);
         Marathon marathon = entityManager.find(Marathon.class, marathonId);
+        entityManager.refresh(marathon);
         Set<User> userList = marathon.getUsers();
         assertTrue(userList != null && userList.size() >= 1);
         User savedUser = userList.stream().filter(i -> emai.equals(i.getEmail())).collect(Collectors.toList()).get(0);
+        assertNotNull(savedUser.getId());
         assertEquals(fisrtName, savedUser.getFirstName());
         assertEquals(lastName, savedUser.getLastName());
         assertEquals(password, savedUser.getPassword());
@@ -66,9 +71,28 @@ public class UserServiceDBTest {
     public void testRemoveUserFromMarathon() {
         Long userId = 1L;
         Long marathonId = 1L;
-        int initialUserMarathonCount = userService.getUserById(userId).getMarathons().size();
+        long initialUserCount = count("User");
+        long initialMarathonCount = count("Marathon");
+        int initialUserMarathonCount = entityManager.find(User.class, userId).getMarathons().size();
         userService.removeUserFromMarathon(userId, marathonId);
-        int currentUserMarathonCount = userService.getUserById(userId).getMarathons().size();
-        assertEquals(initialUserMarathonCount, currentUserMarathonCount);
+        entityManager.flush();
+        long currentUserCount = count("User");
+        long currentMarathonCount = count("Marathon");
+        User currentUser =  entityManager.find(User.class, userId);
+        entityManager.refresh(currentUser);
+        int currentUserMarathonCount = currentUser.getMarathons().size();
+        assertEquals(initialUserCount, currentUserCount);
+        assertEquals(initialMarathonCount, currentMarathonCount);
+        assertEquals(initialUserMarathonCount - 1, currentUserMarathonCount);
+    }
+
+    /**
+     * Returns total count of given entities
+     *
+     * @param entityName - entity name
+     * @return total items saved in DB
+     */
+    protected long count(String entityName) {
+        return entityManager.getEntityManager().createQuery(String.format(COUNT_QUERY_STRING, entityName), Long.class).getSingleResult();
     }
 }
