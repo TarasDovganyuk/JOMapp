2b682ba64603ca60ff52abc9264c45e85b726920
Tamara Kyrychenko <tamara.kyrychenko@gmail.com>
2020-08-01 17:12:22 +0300

    Added logging for student controller

--- src/main/java/com/softserve/edu/jom/controller/StudentController.java
+++ src/main/java/com/softserve/edu/jom/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.softserve.edu.jom.controller;
 
 import com.softserve.edu.jom.model.User;
 import com.softserve.edu.jom.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
     private UserService userService;
 
     @Autowired
@@ -24,12 +27,14 @@ public class StudentController {
 
     @GetMapping("/students")
     public String getUsers(Model model) {
+        logger.info("Get all students");
         model.addAttribute("students", userService.getAllByRole("Trainee"));
         return "students";
     }
 
     @PostMapping("/addStudent")
     public String addStudent(Model model, @ModelAttribute(name = "student") User user) {
+        logger.info("Add new student %s %s", user.getFirstName(), user.getLastName());
         user.setRole(User.Role.TRAINEE);
         userService.createOrUpdateUser(user);
         return "redirect:/students";
@@ -38,6 +43,7 @@ public class StudentController {
 
     @GetMapping("/addStudent")
     public String showAddStudentPage(Model model) {
+        logger.info("Show add new student page");
         User user = new User();
         model.addAttribute("user", user);
         model.addAttribute("mode", "Add");
@@ -46,6 +52,7 @@ public class StudentController {
 
     @GetMapping("/students/{marathon_id}")
     public String getStudentsByMarathonId(@PathVariable(name = "marathon_id") Long marathon_id, Model model) {
+        logger.info("Get all students for marathon with id=%d", marathon_id);
         List<User> userList = userService.getAllByRoleAndMarathonId(User.Role.TRAINEE.name(), marathon_id);
         model.addAttribute("students", userList);
         model.addAttribute("marathonId", marathon_id);
@@ -54,12 +61,14 @@ public class StudentController {
 
     @GetMapping("/students/{marathon_id}/delete/{student_id}")
     public String deleteStudentFromMarathon(@PathVariable(name = "marathon_id") Long marathon_id, @PathVariable(name = "student_id") Long student_id) {
+        logger.info("Remove student with id=%d from marathon with id=%d", student_id, marathon_id);
         userService.removeUserFromMarathon(student_id, marathon_id);
         return "redirect:/students/" + marathon_id;
     }
 
     @PostMapping("/students/{marathon_id}/add")
     public String saveStudentToMarathon(@ModelAttribute(name = "student") User user, @PathVariable(name = "marathon_id") Long marathon_id) {
+        logger.info("Add new student to marathon with id=%d", marathon_id);
         user.setRole(User.Role.TRAINEE);
         userService.addUserToMarathon(user, marathon_id);
         return "redirect:/students/" + marathon_id;
@@ -67,6 +76,7 @@ public class StudentController {
 
     @GetMapping("/students/{marathon_id}/add")
     public String addStudentToMarathon(Model model, @PathVariable(name = "marathon_id") Long marathon_id) {
+        logger.info("Show page to add new student to marathon with id=%d", marathon_id);
         User user = new User();
         model.addAttribute("user", user);
         model.addAttribute("marathonId", marathon_id);
@@ -76,6 +86,7 @@ public class StudentController {
 
     @GetMapping("/students/{marathon_id}/edit/{student_id}")
     public String showEditStudentPage(Model model, @PathVariable(name = "marathon_id") Long marathon_id, @PathVariable(name = "student_id") Long student_id) {
+        logger.info("Show edit student page for student with id=%d and marathon with id=%d", student_id, marathon_id);
         User user = userService.getUserById(student_id);
         model.addAttribute("user", user);
         model.addAttribute("mode", "Edit");
@@ -85,6 +96,7 @@ public class StudentController {
 
     @PostMapping("/students/{marathon_id}/edit/{student_id}")
     public String editStudentPage(Model model, @PathVariable(name = "marathon_id") Long marathon_id, @PathVariable(name = "student_id") Long student_id, @ModelAttribute(name = "student") User user) {
+        logger.info("Edit student with id=%d and marathon with id=%d", student_id, marathon_id);
         user.setId(student_id);
         user.setRole(User.Role.TRAINEE);
         userService.createOrUpdateUser(user);
